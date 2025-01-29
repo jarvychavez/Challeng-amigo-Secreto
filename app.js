c5ba22831ddf05d35762dfd92256d6c1773993b9
jarvychavez <jav.chavez94@gmail.com>
2025-01-28 20:59:14 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
  // Crea un array para almacenar los nombres
-let amigos = [];
-let intentos = 0;
-
-
-//Capturar el valor del campo de entrada
-function agregarAmigo() {
-    let nuevoAmigo = document.getElementById("amigo").value;
-
-    //Verificar si el campo esta vacío
-    if(inputNombre === ""){
-        alert("Por favor ingrese un nombre");
-        return;
-    }
-
+ let amigos = [];
+ let intentos = 0;
+ 
+ //Capturar el valor del campo de entrada
+ function agregarAmigo() {
+     let nuevoAmigo = document.getElementById("amigo").value.trim(); // Eliminar espacios en blanco
+ 
+     //Verificar si el campo esta vacío
+     if(nuevoAmigo === ""){
+         alert("Por favor ingrese un nombre");
+         return;
+     }
+ 
+     //Agregar el nuevo amigo
+     amigos.push(nuevoAmigo);
+ 
+     //Actualiza la lista de amigos en el la pagina
+     actualizarListaAmigos();
+ 
+     document.getElementById("amigo").value = ""; // Limpiar el campo de entrada
  }
-
-   //Agregar el nuevo amigo
-   amigos.push(nuevoAmigo);
-
-   //Actualiza la lista de amigos en el la pagina
-   actualizarListaAmigos();
-
-   document.getElementById("amigo").value="";
-
-   
-
-    
-  //Funcion para actualizar los amigos
-function actualizarListaAmigos(){
-    let listaAmigos = document.getElementById("listaAmigos");
-    listaAmigos.innerHTML = "";
-}
+ 
+ //Funcion para actualizar los amigos
+ function actualizarListaAmigos(){
+     let listaAmigos = document.getElementById("listaAmigos");
+     listaAmigos.innerHTML = ""; // Limpiar la lista
+ 
+     // Iterar sobre el arreglo
+     for (let i = 0; i < amigos.length; i++) {
+         // Crear elementos de lista
+         let nuevoLi = document.createElement("li");
+         nuevoLi.textContent = amigos[i];
+ 
+         // Agregar elementos a la lista
+         listaAmigos.appendChild(nuevoLi);
+     }
+ }
